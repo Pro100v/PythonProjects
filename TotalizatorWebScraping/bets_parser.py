3e9c1eb761344f63950beed3f2aeffb8c95550cd
pro100v <mr.deff@gmail.com>
2020-03-10 03:58:51 +0200

    worked version with parsing live ligastavok

--- TotalizatorWebScraping/bets_parser.py
+++ TotalizatorWebScraping/bets_parser.py
@@ -12,13 +12,11 @@ from selenium.common.exceptions import TimeoutException
 import pandas as pd
 from lxml import html
 
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import logging
 import re
-
 
 import settings
 import bets_utils
@@ -37,7 +35,8 @@ class BetParser(object):
         Args:
 
         """
-        logging.debug(f"Создается экземпляр {self.__class__.__name__} с параметрами webdriver:{webdriver.name} url:{url}")
+        logging.debug(
+            f"Создается экземпляр {self.__class__.__name__} с параметрами webdriver:{webdriver.name} url:{url}")
         assert webdriver and url
         self.driver = webdriver
         self.url = url
@@ -45,14 +44,13 @@ class BetParser(object):
         self.idling = 0
         self.soup = None
 
-
-        #создаем вкладку в которой будет загружаться веб-страница
-        self.tab_name = "test"  #ToDo: придумать генератор названия вкладки
+        # создаем вкладку в которой будет загружаться веб-страница
+        self.tab_name = "test"  # ToDo: придумать генератор названия вкладки
         js = f"window.open('{self.url}', '{self.tab_name}');"
         logging.debug(f"Создаем новую вкладку скриптом js: {js}")
         self.driver.execute_script(js)
 
-        #TODO: в класс передавать родительский объект и в него записывать себя для возможности управления из родителя
+        # TODO: в класс передавать родительский объект и в него записывать себя для возможности управления из родителя
 
     def process(self):
         raise NotImplementedError()
@@ -83,8 +81,6 @@ class BetParser(object):
             logging.debug(f"Закрываем вкладку браузера tab:{self.tab_name}")
             self.driver.switch_to.window(self.tab_name)
             self.driver.close()
-            self.driver.quit()
-
 
     @classmethod
     def execute(cls, webdriver: webdriver, url: str):
@@ -96,13 +92,12 @@ class BetParser(object):
             time.sleep(settings.BET_SLEEP)
 
 
-
-
-class Ligastavok(BetParser):
+class LigastavokProbe(BetParser):
     """
     Парсер ставок Ligastavok конкретного события
     site https://www.ligastavok.ru
     """
+
     def process(self):
         self.driver.switch_to.window(self.tab_name)
         # print(self.html)
@@ -122,32 +117,136 @@ class Ligastavok(BetParser):
         self.soup = BeautifulSoup(str(self.html), "lxml")
 
 
+class BaseBetParser(object):
+    """
+    парсер страничек с лайвом
+    """
+
+    def __init__(self, web_driver: webdriver, url: str):
+        logging.debug(
+            f"Создается экземпляр {self.__class__.__name__} с параметрами webdriver:{web_driver.name} url:{url}")
+        # assert webdriver and url
+        self.driver = web_driver
+        self.url = url
+        self.errors = {}
+        self.idling = 0
+        pass
+
+    def process(self):
+        raise NotImplementedError
+
+    def prepare(self):
+        pass
+
+    def check_changes(self):
+        logging.debug(f"Проверяем состояние изменеия параметра ставок")
+        self.idling += 1
+
+    @classmethod
+    def execute(cls, wb: webdriver, url: str):
+        instance = cls(wb, url)
+        instance.prepare()
+        while instance.idling < settings.MAX_IDLE:
+            instance.process()
+            instance.check_changes()
+            time.sleep(settings.BET_SLEEP)
+
+
+class LigastavokBase(BaseBetParser):
+    """
+    базовый класс для сайта https://www.ligastavok.ru
+    """
+
+    def prepare(self):
+        logging.debug(f"Переходим по url:{self.url}")
+        self.driver.get(self.url)
+
+        timeout = 30
+        logging.debug(f"ждем в течении {timeout} сек. для загрузки контента")
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.ID, "content")))
+        except TimeoutException:
+            logging.debug(f"время ожидания загрузки контента превысело {timeout} сек. ")
+            # self.errors.  todo: добавить сообщение об ошибке. Понять почему ошибки словарем сделаны
+            self.idling = settings.MAX_IDLE
+            return
+        logging.debug("данные успешно загруженны")
+
+
+class LigastavokLive(LigastavokBase):
+    def __init__(self, web_driver: webdriver, url: str):
+        super(LigastavokLive, self).__init__(web_driver=web_driver, url=url)
+        self.bet_matches = []
+
+    def process(self):
+        def clr_txt(s: str) -> str:
+            return s.replace('\\n', '').replace('\\t', '').replace("', '", '').strip()
+        logging.debug(f"Загружаем контент в BeautifulSoup")
+        # b_s = BeautifulSoup(self.get_content(), "lxml")
+        b_s = BeautifulSoup(self.get_content(), "html.parser")
+        all_events = b_s.find_all('div', class_=re.compile('bui-event-row'), itemtype="http://schema.org/Event")
+        for event in all_events:
+            main = event.find('div', class_=re.compile('column_main'))
+            league = main.find('div', class_=re.compile('league')).get_text()
+            league = clr_txt(league)
+            status = (main.find('div', class_=re.compile('status')).get_text())
+            status = clr_txt(status)
+            info = main.find('div', class_=re.compile('info'))
+            href = info.a.get('href')
+            teams = info.a.find_all('span')
+            home_team = teams[0].string
+            guest_team = teams[1].string
+            current_score = info.find('div', re.compile('current'))
+            current_score = current_score.get_text() if current_score else ''
+            total_score = info.find('div', re.compile('total'))
+            total_score = total_score.get_text() if total_score else ''
+            outcomes = event.find('div', re.compile('column_outcome'))
+            bets = []
+            for outcome in outcomes.find_all('div', re.compile('bui-group-outcome__wrapper')):
+                bets.append([clr_txt(element.string) for element in outcome.div.contents if element.name == 'div'])
+            print(f"league:{league}; status:{status}; href:{href}; home_team:{home_team}; guest_team:{guest_team}; "
+                  f"current_score:{current_score}; total_score:{total_score}; bets:{bets}")
+
+    def prepare(self):
+        super(LigastavokLive, self).prepare()
+
+    def get_content(self) -> str:
+        # file = os.path.join(bets_utils.app_path(), "pages", "ligastavok_live.html")
+        # with open(file, 'r') as f:
+        #     return str(f.readlines())
+        return self.driver.find_element_by_id("content").get_attribute('innerHTML')
+
+
 def main():
     bets_utils.set_logging(logging.DEBUG)
     url = "https://www.ligastavok.ru/bets/live"
-    url = 'about:blank'
-    
-    driver_path = settings.PATH_CHROMEDRIVER.split('/')
-    # driver_path = os.path.join(os.getcwd(), *driver_path)
-    self_path = os.path.dirname(os.path.abspath(__file__))
-    driver_path = os.path.join(self_path, *driver_path)
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    logging.debug(f"Старт программы с параметрами driver_path:{driver_path},  url:{url}")
-    if os.path.exists(driver_path):
-        # print(driver_path)
+    # url = 'about:blank'
 
-        # service = Service(driver_path)
-        # service.start()
-        # wd = webdriver.Remote(service.service_url)
+    driver_path = bets_utils.get_webdriver_path(browser=bets_utils.CHROME)
+    options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--disable-notifications")
+    # options.add_argument("--enable-automation")
+    # proxy_str = '--proxy-server=http://{0}:{1}'.format(
+    #     PROXY_HOST, str(PROXY_PORT))
+    # options.add_argument(proxy_str)
+    # Ставим запрет на загрузку картинок
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
+
+    logging.debug(f"Старт программы с параметрами driver_path:{driver_path}, url:{url}")
+    if os.path.exists(driver_path):
         wd = webdriver.Chrome(
-            executable_path=driver_path, 
-            # service_log_path=os.path.join(self_path, 'logs'),
-            options=options
+            executable_path=driver_path,
+            options=options,
         )
-        Ligastavok.execute(wd, url)
+        # LigastavokProbe.execute(wd, url)   # парсер детальных данных
+        LigastavokLive.execute(wd, url)
         if wd:
             wd.quit()
     else:
--- TotalizatorWebScraping/bets_utils.py
+++ TotalizatorWebScraping/bets_utils.py
@@ -1,12 +1,42 @@
 import logging
+import os
+import platform
+
+import settings
+
+CHROME = "chrome".upper()
+FIREFOX = "firefox".upper()
+
 
 def set_logging(logging_level=logging.INFO):
     format = "%(asctime)s (%(threadName)-9s): %(message)s"
     # log_lvl = logging.INFO  # logging.INFO /  logging.DEBUG  / logging.WARNING
     try:
         log_lvl = logging._checkLevel(logging_level)
-    except:
+    except Exception as exp:
         log_lvl = logging.INFO
 
     logging.basicConfig(format=format, level=log_lvl,
                         datefmt="%d/%m/%Y %H:%M:%S")
+
+
+def get_webdriver_path(browser=CHROME) -> str:
+    system_version = platform.system().upper()
+    driver_path = ""
+    if browser == CHROME:
+        driver_path = settings.PATH_CHROMEDRIVER.split('/')
+    if browser == FIREFOX:
+        raise NotImplementedError()
+    assert driver_path
+
+    parent = app_path()
+    if system_version.startswith("LINUX"):
+        return os.path.join(parent, *driver_path, "chromedriver")
+    if system_version.startswith("WINDOWS"):
+        return os.path.join(parent, *driver_path, "chromedriver.exe")
+    if system_version.startswith("DARWIN"):
+        return os.path.join(parent, *driver_path, "chromedriver-mac")
+
+
+def app_path():
+    return os.path.dirname(os.path.abspath(__file__))
